a60153153f92d5c4dbe6fc59a6f0cfe7db8fbfd8
Görkem Acar <grkemacar@outlook.com>
2020-09-01 13:49:28 +0300

    Added mvc top application

--- glory.BookStore/glory.BookStore/Startup.cs
+++ glory.BookStore/glory.BookStore/Startup.cs
@@ -16,24 +16,28 @@ namespace glory.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //all the features and despendencies...
+
+            services.AddMvc();//controller model kullanýlabilir.
+            services.AddControllersWithViews();//controller model view kullanýlabilir.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //http line
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute(); //defaullt home index controllerine gider.
             });
         }
     }
